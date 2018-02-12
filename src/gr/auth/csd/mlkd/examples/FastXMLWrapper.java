abd7d6c89acfd9f56dff2920b89f43614f8bd22e
ypapanik <ypapanik@csd.auth.gr>
2018-02-12 21:30:03 +0200

    added FastXML comparison

--- src/gr/auth/csd/mlkd/examples/FastXMLWrapper.java
+++ src/gr/auth/csd/mlkd/examples/FastXMLWrapper.java
@@ -5,6 +5,11 @@
  */
 package gr.auth.csd.mlkd.examples;
 
+import gr.auth.csd.mlkd.evaluation.EvaluateFastXML;
+import gr.auth.csd.mlkd.preprocessing.CorpusJSON;
+import gr.auth.csd.mlkd.preprocessing.Dictionary;
+import gr.auth.csd.mlkd.preprocessing.Labels;
+import gr.auth.csd.mlkd.preprocessing.VectorizeMultiLabelLibSVM;
 import java.io.File;
 import java.io.IOException;
 
@@ -15,18 +20,26 @@ import java.io.IOException;
 public class FastXMLWrapper {
 
     public static void main(String args[]) throws InterruptedException, IOException {
-        eval();
-        //runFastXML();
+        String dataset = args[0];
+        String inTrain = "data/" + args[0] + "/" + args[0] + "train";
+        String inTest = "data/" + args[0] + "/" + args[0] + "test";
+        String outTrain = "data/" + args[0] + "/train.txt";
+        String outTest = "data/" + args[0] + "/test.txt";
+
+        CorpusJSON corpus = new CorpusJSON(inTrain);
+        CorpusJSON corpus2 = new CorpusJSON(inTest);
+        Dictionary dic = new Dictionary(corpus, 5, 10000, 10, 5000);
+        Labels labels = new Labels(corpus);
+
+        VectorizeMultiLabelLibSVM v = new VectorizeMultiLabelLibSVM(dic, false, labels);
+        v.transfrom(inTrain, inTest, outTrain, outTest);
+        //Process process = new ProcessBuilder("./fastXML.sh", dataset).redirectError(new File("err.txt")).redirectOutput(new File("out.txt")).start();
+        Process process = new ProcessBuilder("./pfastreXML.sh", dataset).redirectError(new File("err.txt")).redirectOutput(new File("out.txt")).start();
+        process.waitFor();
+        EvaluateFastXML ev = new EvaluateFastXML(outTest, "results/" + dataset + "/score_mat.txt", labels.getSize(), 5);
+
+        process = new ProcessBuilder("./eval.sh", "bibtex", "predictions").redirectError(new File("err.txt")).redirectOutput(new File("out.txt")).start();
+        process.waitFor();
     }
 
-    public static void runFastXML() throws IOException, InterruptedException {
-        Process process = new ProcessBuilder("./fastXML.sh", "EUR-Lex").redirectError(new File("err.txt")).redirectOutput(new File("out.txt")).start();
-        process.waitFor();
-    }
-    
-    public static void eval() throws IOException, InterruptedException {
-        Process process = new ProcessBuilder("./eval.sh", "bibtex", "predictions").redirectError(new File("err.txt")).redirectOutput(new File("out.txt")).start();
-        process.waitFor();
-    }
-    
 }
